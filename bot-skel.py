49a34f51177845ef2aeb0efb6dc9fcc91eb60da7
Petrica Vladimir <119428185+petrica913@users.noreply.github.com>
2022-12-20 12:44:35 +0200

    Add files via upload

---
+++ bot-skel.py
@@ -0,0 +1,176 @@
+#!./.venv/bin/python
+import discord      # base discord module
+import code         # code.interact
+import os           # environment variables
+import inspect      # call stack inspection
+import random       # dumb random number generator
+from discord.ext import commands    # Bot class and utils
+################################################################################
+############################### HELPER FUNCTIONS ###############################
+################################################################################
+# log_msg - fancy print
+#   @msg   : string to print
+#   @level : log level from {'debug', 'info', 'warning', 'error'}
+def log_msg(msg: str, level: str):
+    # user selectable display config (prompt symbol, color)
+    dsp_sel = {
+        'debug'   : ('\033[34m', '-'),
+        'info'    : ('\033[32m', '*'),
+        'warning' : ('\033[33m', '?'),
+        'error'   : ('\033[31m', '!'),
+    }
+    # internal ansi codes
+    _extra_ansi = {
+        'critical' : '\033[35m',
+        'bold'     : '\033[1m',
+        'unbold'   : '\033[2m',
+        'clear'    : '\033[0m',
+    }
+    # get information about call site
+    caller = inspect.stack()[1]
+    # input sanity check
+    if level not in dsp_sel:
+        print('%s%s[@] %s:%d %sBad log level: "%s"%s' % \
+            (_extra_ansi['critical'], _extra_ansi['bold'],
+             caller.function, caller.lineno,
+             _extra_ansi['unbold'], level, _extra_ansi['clear']))
+        return
+    # print the damn message already
+    print('%s%s[%s] %s:%d %s%s%s' % \
+        (_extra_ansi['bold'], *dsp_sel[level],
+         caller.function, caller.lineno,
+         _extra_ansi['unbold'], msg, _extra_ansi['clear']))
+################################################################################
+############################## BOT IMPLEMENTATION ##############################
+################################################################################
+# bot instantiation
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Bot(command_prefix='!', intents=intents)
+# on_ready - called after connection to server is established
+@bot.event
+async def on_ready():
+    log_msg('logged on as <%s>' % bot.user, 'info')
+# on_message - called when a new message is posted to the server
+#   @msg : discord.message.Message
+@bot.event
+async def on_message(msg):
+    # filter out our own messages
+    #if msg.author == bot.user:
+    #    return
+    log_msg('message from <%s>: "%s"' % (msg.author, msg.content), 'debug')
+    # overriding the default on_message handler blocks commands from executing
+    # manually call the bot's command processor on given message
+    await bot.process_application_commands(msg)
+# roll - rng chat command
+#   @ctx     : command invocation context
+#   @max_val : upper bound for number generation (must be at least 1)
+@bot.command(brief='Generate random number between 1 and <arg>')
+async def roll(ctx, max_val: int):
+    # argument sanity check
+    if max_val < 1:
+        raise Exception('argument <max_val> must be at least 1')
+    await ctx.send_response(random.randint(1, max_val))
+# roll_error - error handler for the <roll> command
+#   @ctx     : command that crashed invocation context
+#   @error   : ...
+@roll.error
+async def roll_error(ctx, error):
+    await ctx.send_response(str(error))
+################################################################################
+############################# PROGRAM ENTRY POINT ##############################
+################################################################################
+if __name__ == '__main__':
+    # check that token exists in environment
+    if 'BOT_TOKEN' not in os.environ:
+        log_msg('save your token in the BOT_TOKEN env variable!', 'error')
+        exit(-1)
+    # launch bot (blocking operation)
+    bot.run(os.environ['BOT_TOKEN'])
+
+from youtube_dl import YoutubeDL
+
+class music_cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+        #all the music related stuff
+        self.is_playing = False
+        self.is_paused = False
+
+        # 2d array containing [song, channel]
+        self.music_queue = []
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        self.vc = None
+
+     #searching the item on youtube
+    def search_yt(self, item):
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            try: 
+                info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
+            except Exception: 
+                return False
+
+        return {'source': info['formats'][0]['url'], 'title': info['title']}
+
+    def play_next(self):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            #get the first url
+            m_url = self.music_queue[0][0]['source']
+
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    # infinite loop checking 
+    async def play_music(self, ctx):
+        if len(self.music_queue) > 0:
+            self.is_playing = True
+
+            m_url = self.music_queue[0][0]['source']
+            
+            #try to connect to voice channel if you are not already connected
+            if self.vc == None or not self.vc.is_connected():
+                self.vc = await self.music_queue[0][1].connect()
+
+                #in case we fail to connect
+                if self.vc == None:
+                    await ctx.send("Could not connect to the voice channel")
+                    return
+            else:
+                await self.vc.move_to(self.music_queue[0][1])
+            
+            #remove the first element as you are currently playing it
+            self.music_queue.pop(0)
+
+            self.vc.play(discord.FFmpegPCMAudio(m_url, **self.FFMPEG_OPTIONS), after=lambda e: self.play_next())
+        else:
+            self.is_playing = False
+
+    @commands.command(name="play", aliases=["p","playing"], help="Plays a selected song from youtube")
+    async def play(self, ctx, *args):
+        query = " ".join(args)
+        
+        voice_channel = ctx.author.voice.channel
+        if voice_channel is None:
+            #you need to be connected so that the bot knows where to go
+            await ctx.send("Connect to a voice channel!")
+        elif self.is_paused:
+            self.vc.resume()
+        else:
+            song = self.search_yt(query)
+            if type(song) == type(True):
+                await ctx.send("Could not download the song. Incorrect format try another keyword. This could be due to playlist or a livestream format.")
+            else:
+                await ctx.send("Song added to the queue")
+                self.music_queue.append([song, voice_channel])
+                
+                if self.is_playing == False:
+                    await self.play_music(ctx)
